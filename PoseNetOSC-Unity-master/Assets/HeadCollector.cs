6eeba250891a78eec2f749790cbcedb00bc72822
GuillemPuente <59957213+GuillemPuente@users.noreply.github.com>
2020-05-07 20:33:09 +0200

    ·rd commit

---
+++ PoseNetOSC-Unity-master/Assets/HeadCollector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadCollector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("HeadCollect"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
