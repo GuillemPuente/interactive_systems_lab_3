43b20bebc2cb686d92b9d1ad6c65b083c4991f03
GuillemPuente <59957213+GuillemPuente@users.noreply.github.com>
2020-05-07 20:10:35 +0200

    Second commit

---
+++ PoseNetOSC-Unity-master/Assets/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
